37c820ceb07c6600c2f0eb90d15eee2c1867d04a
infinitumnihilum <jrdavis1989@gmail.com>
2016-03-31 12:05:44 -0400

    First commit

---
+++ src/main/java/com/hello/Quote.java
@@ -0,0 +1,44 @@
+package com.hello;
+
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+/**
+ * Created by jrdavis on 3/3/16.
+ */
+
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public class Quote {
+
+        private String type;
+        private Value value;
+
+        public Quote() {
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public Value getValue() {
+            return value;
+        }
+
+        public void setValue(Value value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Quote{" +
+                    "type='" + type + '\'' +
+                    ", value=" + value +
+                    '}';
+        }
+    }
+
---
+++ src/main/java/com/hello/Value.java
@@ -0,0 +1,39 @@
+package com.hello;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+/**
+ * Created by jrdavis on 3/3/16.
+ */
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Value {
+
+    private Long id;
+    private String quote;
+
+    public Value() {
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getQuote() {
+        return this.quote;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+
+    @Override
+    public String toString() {
+        return "Value{" +
+                "id=" + id +
+                ", quote='" + quote + '\'' +
+                '}';
+    }
+}
